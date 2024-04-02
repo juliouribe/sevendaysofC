a3c09b7bbc91acfa01b727f997ec3d7a0c0e5d08
Wizzerrd <luisthelaffitte@gmail.com>
2024-04-02 13:25:57 -0700

    add for loop

--- chapter1/code/cToF.c
+++ chapter1/code/cToF.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* print Fahrenheit-Clesius table
     for fahr = 0, 20, ..., 300;
 */
 int main() {
     float lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
 
     float celsius;
     float fahr = lower; /* declaration and assignment in one line */
     
+    // While Loop
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         // In 3.0f, the 3 stands for 3 wide and the 0 after the decimal place
@@ -20,4 +22,10 @@ int main() {
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+
+    // For Loop
+    // for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+    //     printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    // }
+    
 }
