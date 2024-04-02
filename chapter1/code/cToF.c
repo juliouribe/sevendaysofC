19874657918f083c2bc0f04f0af433171797dd0f
Wizzerrd <luisthelaffitte@gmail.com>
2024-04-02 13:29:03 -0700

    constant declarations, add flair

--- chapter1/code/cToF.c
+++ chapter1/code/cToF.c
@@ -8,24 +8,27 @@
     for fahr = 0, 20, ..., 300;
 */
 int main() {
-    float lower, upper, step;
-
-
     float celsius;
-    float fahr = lower; /* declaration and assignment in one line */
+    float fahr = LOWER; /* declaration and assignment in one line */
     
+    printf("While Loop\n");
+    printf("###########\n");
+
     // While Loop
-    while (fahr <= upper) {
+    while (fahr <= UPPER) {
         celsius = (5.0/9.0) * (fahr-32.0);
         // In 3.0f, the 3 stands for 3 wide and the 0 after the decimal place
         // instead of %d we use %f for floats? 6.1f allows one value after the decimal place.
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 
+    printf("\nFor Loop\n");
+    printf("###########\n");
+
     // For Loop
-    // for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
-    //     printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    // }
+    for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+        printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
     
 }
