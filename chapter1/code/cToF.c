5d419da664a3c45ccb0b9c2a164253f10fd3a091
Wizzerrd <luisthelaffitte@gmail.com>
2024-04-02 13:11:11 -0700

    add declaration and assignment in same line for fahr

--- chapter1/code/cToF.c
+++ chapter1/code/cToF.c
@@ -4,14 +4,14 @@
     for fahr = 0, 20, ..., 300;
 */
 int main() {
-    float fahr, celsius;
+    float celsius;
     float lower, upper, step;
 
     lower = 0;
     upper = 300;
     step = 20;
 
-    fahr = lower;
+    float fahr = lower; /* declaration and assignment in one line */
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         // In 3.0f, the 3 stands for 3 wide and the 0 after the decimal place
