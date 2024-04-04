57ffe915f6b1d861c06142daa5930121bcaf761d
Julio Uribe <uribejulioc@gmail.com>
2024-04-03 18:46:30 -0700

    Add logicLoop.c

---
+++ chapter2/code/logicLoop.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, lim, c, count;
+    lim = 5;
+    char t[lim];
+    char s[lim];
+    // test
+    // for (i=0; i < lim-1 && (c=getchar()) != '\n' && c != EOF; ++i)
+    //    t[i] = c;
+    // printf("\n");
+    // second time
+    i = 0;
+    while (c != EOF){
+        c = getchar();
+        if (c != '\n'){
+            s[i] = c;
+            ++i;
+            ++count;
+        }
+        if (i == lim) break;
+    }
+    // printf("\n");
+    // for (i = 0; i < lim-1; ++i) {
+    //     putchar(t[i]);
+    // }
+    printf("\n");
+    for (i = 0; i < count; ++i) {
+        putchar(s[i]);
+    }
+    printf("\n");
+    return 0;
+}
