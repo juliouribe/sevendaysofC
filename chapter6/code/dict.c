039c14975cde633437ef8c3641142270a75f0ea3
Wizzerrd <luisthelaffitte@gmail.com>
2024-04-12 18:39:42 -0700

    add struct

---
+++ chapter6/code/dict.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define HASHSIZE 101
+
+struct nlist { /* table entry: */
+    struct nlist *next; /* next entry in chain */
+    char *name; /* defined name */
+    char *defn; /* replacement text */
+};
+static struct nlist *hashtab[HASHSIZE]; /* pointer table */ 
+
+unsigned hash(char *s);
+struct nlist *lookup(char *s);
+struct nlist *install(char *name, char *defn);
+
+int main(){
+    char str1[] = install("fruit", "banana")->name;
+    char str2[] = install("fruit", "banana")->defn;
+    // printf();
+    return 0;
+}
+
+unsigned hash(char *s)
+{
+    unsigned hashval;
+    for (hashval = 0; *s != '\0'; s++)
+        hashval = *s + 31 * hashval;
+    return hashval % HASHSIZE;
+} 
+
+struct nlist *lookup(char *s)
+{
+    struct nlist *np;
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+        if (strcmp(s, np->name) == 0)
+            return np; /* found */
+    return NULL; /* not found */
+} 
+
+struct nlist *install(char *name, char *defn)
+{
+    struct nlist *np;
+    unsigned hashval;
+    if ((np = lookup(name)) == NULL) { /* not found */
+        np = (struct nlist *) malloc(sizeof(*np)); 
+        if (np == NULL || (np->name = strdup(name)) == NULL)
+            return NULL;
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else /* already there */
+        free((void *) np->defn); /*free previous defn */
+    if ((np->defn = strdup(defn)) == NULL)
+        return NULL;
+    return np;
+}
